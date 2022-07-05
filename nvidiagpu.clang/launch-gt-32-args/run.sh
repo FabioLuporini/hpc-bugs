bf2377183753c641c21d5e22a975a1d2dc7a355e
FabioLuporini <fabilupo@gmail.com>
2022-07-05 18:32:48 +0200

    finalize gt32 MFE

--- nvidiagpu.clang/launch-gt-32-args/run.sh
+++ nvidiagpu.clang/launch-gt-32-args/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 clear
-OMP_TARGET_OFFLOAD=MANDATORY clang -Xopenmp-target -march=sm_70 -fopenmp -fopenmp-targets=nvptx64 gt32.c -o gt32
+OMP_TARGET_OFFLOAD=MANDATORY clang -Xopenmp-target -march=sm_86 -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda gt32.c -o gt32
+./gt32
