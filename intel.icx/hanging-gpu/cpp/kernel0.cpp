9af7615cea97dbc50f6e33ff29377a89c41af01d
Fabio Luporini <fabio@devitocodes.com>
2023-12-18 08:16:12 -0800

    Add intel+sycl hanging GPU test

---
+++ intel.icx/hanging-gpu/cpp/kernel0.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "stdlib.h"
+#include "math.h"
+#include "sys/time.h"
+#include <sycl/sycl.hpp>
+
+#define uL0(t,x,y) u[(t)*x_stride0 + (x)*y_stride0 + (y)]
+
+using namespace sycl;
+
+extern "C" void foo();
+
+void foo()
+{
+  printf("Running foo()... \n");
+
+  queue qid0(gpu_selector_v);
+
+  const unsigned long nbytes = 500*500*500*4;
+
+  float *vp = (float*) aligned_alloc(64, nbytes);
+  float *d_vp = (float *)aligned_alloc_device(4096,nbytes,qid0);
+
+  qid0.memcpy(d_vp,vp,nbytes);
+  qid0.wait();
+
+  free(vp);
+  free(d_vp,qid0);
+  qid0.wait();
+}
